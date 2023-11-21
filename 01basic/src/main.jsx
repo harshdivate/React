de32bd48c51a7380bd2cc0cdbfc5068ab9f405f9
Harsh <harshdivate45@gmail.com>
2023-11-21 16:49:14 +0530

    Props

--- 01basic/src/main.jsx
+++ 01basic/src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <div>Custom app</div>
+  )
+}
+
+// const reactElement = {
+//   type : 'a',
+//   props : {
+//       href : 'https://google.com',
+//       target: '_blank'
+//   },
+//   children : 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href : "https://google.com",
+    target : '_blank'
+  },
+  'Click me to visit google'
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ 
+   reactElement
+ 
 )
