8ba18040efb64c36968446b533a7135fc2e01845
Harsh <harshdivate45@gmail.com>
2023-11-27 21:16:30 +0530

    Movie Page

---
+++ part-1-practice/src/hooks/useMovieData.js
@@ -0,0 +1,28 @@
+import React,{useEffect, useState} from "react";
+
+
+function useMovieData (d) {
+    const [data,setData] = useState({})
+    useEffect(()=>{
+        const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDZkMzRhZWQ5MGVlZDU4MjI4NTFkZDFjNGIyOTA3MCIsInN1YiI6IjY1NDg3YjhhOTI0Y2U2MDEwMWY1MzMwYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uXa14BGj3_FUJLeWvvi0XQ1ZSuqS_LSHC0e03NwoUrs'
+            }
+          };
+          
+          fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+            .then(response => response.json())
+            .then(response =>{
+                const movies= response.results[0];
+                setData(movies);
+                console.log(data);
+            })
+            .catch(err => console.error(err));
+        return data;
+    },[d])
+   
+}
+
+export default useMovieData;
