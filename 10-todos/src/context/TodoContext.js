f87b6b360ccb890edf28a6e5d0b3c6bd9b048625
Harsh <harshdivate45@gmail.com>
2023-11-30 20:24:59 +0530

    Todos

---
+++ 10-todos/src/context/TodoContext.js
@@ -0,0 +1,23 @@
+import { useContext,createContext  } from "react";
+
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Todo msg",
+            completed: false,
+        },
+    ],
+    addTodos:(todo)=>{},
+    updateTodo:(todo,id)=>{},
+    deleteTodo :(id)=>{},
+    toggleComplete :(id)=>{}
+})
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+
+export const TodoProvider = TodoContext.Provider
